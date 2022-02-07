eafdc6cf9a70d55f2766b0959a2716bc83297a08
shubhm23 <1ms19cs118@gmail.com>
2022-02-07 14:50:06 +0530

    Add User Details on Home

--- app/src/main/java/com/example/loginapplication/MainActivity.java
+++ app/src/main/java/com/example/loginapplication/MainActivity.java
@@ -6,20 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 
 public class MainActivity extends AppCompatActivity {
     Button btnLogOut;
     FirebaseAuth mAuth;
-
+    TextInputEditText loginEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnLogOut = findViewById(R.id.btnLogout);
+        loginEmail = findViewById(R.id.loginEmail);
         mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            String email = "";
+            for (UserInfo profile : user.getProviderData()) {
+                email = profile.getEmail();
+            }
+            loginEmail.setText(email);
+        }
 
         btnLogOut.setOnClickListener(view ->{
             mAuth.signOut();
